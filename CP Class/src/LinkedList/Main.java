266879f699c1a844f51ed66a9da33ce8be3bf04a
konpeeyush <piyushksingh2589@gmail.com>
2022-08-26 08:48:30 +0530

    Adding LL notes

---
+++ CP Class/src/LinkedList/Main.java
@@ -0,0 +1,50 @@
+package LinkedList;
+
+public class Main {
+    private ListNode head;
+
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Main sl = new Main();
+        sl.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        sl.head.next = second;
+        second.next = third;
+        third.next = fourth;
+        sl.display();
+        System.out.println();
+        System.out.print("Length" + sl.length());
+    }
+
+    void display() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    public int length() {
+        if (head == null)
+            return 0;
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
